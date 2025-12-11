87fd95f190cf6e79ba3c924f594544e0b29e7894
Dina Mohamed <dinamashhor409@gmail.com>
2025-12-11 02:43:33 +0200

    Creation of Home Class

---
+++ src/Home.java
@@ -0,0 +1,36 @@
+import javax.media.opengl.*;
+import javax.swing.*;
+import com.sun.opengl.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.media.opengl.glu.GLU;
+import Texture.TextureReader;
+
+// java packages
+import java.io.*;
+import java.util.*;
+import javax.sound.sampled.*;
+
+// our own packages
+import Pages.*;
+
+public class Home extends JFrame {
+    AudioInputStream audioStream;
+    Clip clip;
+
+
+    public Home() {
+        try {
+            audioStream = AudioSystem.getAudioInputStream(new File("Assets\\sound\\game.wav"));
+            clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        class HomeEventListener implements GLEventListener, MouseMotionListener, MouseListener, KeyListener {
+
+            public HomeEventListener(Clip clip) {
+                this.clip = clip;
+            }
+        }
