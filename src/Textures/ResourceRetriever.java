454d9102b60c19159896f8ecf09fa176ea41e502
Dina Mohamed <dinamashhor409@gmail.com>
2025-12-11 03:44:57 +0200

    Creation Of Texture Folder

---
+++ src/Textures/ResourceRetriever.java
@@ -0,0 +1,33 @@
+package Texture;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * Utility class that allows transparent reading of files from
+ * the current working directory or from the classpath.
+ * @author Pepijn Van Eeckhoudt
+ */
+public class ResourceRetriever {
+    @SuppressWarnings("deprecation")
+    public static URL getResource(final String filename) throws IOException {
+        URL url = ClassLoader.getSystemResource(filename);
+        if (url == null) {
+            return new URL("file", "localhost", filename);
+        } else {
+            return url;
+        }
+    }
+
+    public static InputStream getResourceAsStream(final String filename) throws IOException {
+        // Try to load resource from jar
+        InputStream stream = ClassLoader.getSystemResourceAsStream(filename);
+        if (stream == null) {
+            return new FileInputStream(filename);
+        } else {
+            return stream;
+        }
+    }
+}
